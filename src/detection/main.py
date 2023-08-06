4fbb25680b3ea44199b53810c8688121956e64c7
Filbert <92862064+Filbert88@users.noreply.github.com>
2023-08-07 00:04:09 +0700

    update main.py

--- src/detection/main.py
+++ src/detection/main.py
@@ -14,3 +14,11 @@ if count_empty > 0:
     ser.write((str(available_parking_spaces[0] + 100) + '\n').encode('utf-8'))
 
 ser.write((str(count_empty) + '\n').encode('utf-8'))
+
+
+with open('KotakParkir', 'rb') as f:
+posList = pickle.load(f)
+width, height = 80, 120
+while True:
+if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
