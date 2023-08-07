3f1fca2d31b2dbb072905a0ece29904c95c11c46
HananFitS <114347723+HananFitS@users.noreply.github.com>
2023-08-07 07:06:05 +0700

    Update main.py

--- src/detection/main.py
+++ src/detection/main.py
@@ -17,12 +17,14 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 status_list = []
 
-
+def checkParkingSpace(imgPro):
+    count_empty = 0
+    #count_occupied = 0
+    for i, pos in enumerate(posList):
+        x,y = pos
 
 available_parking_spaces = [
-    i for i in range(len(posList)) if
-    cv2.countNonZero(imgPro[posList[i][1]:posList[i][1] + height, posList[i][0]:posList[i][0] + width]) <= 500
-]
+    i for i in range(len(posList)) if cv2.countNonZero(imgPro[posList[i][1]:posList[i][1] + height, posList[i][0]:posList[i][0] + width]) <= 500]
 
 if count_empty == 0:
     ser.write((str(count_empty) + '\n').encode('utf-8'))
@@ -34,8 +36,22 @@ ser.write((str(count_empty) + '\n').encode('utf-8'))
 
 
 with open('KotakParkir', 'rb') as f:
-posList = pickle.load(f)
+    posList = pickle.load(f)
+
 width, height = 80, 120
+
 while True:
-if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+
+    cv2.imshow("Image", img)
+    # cv2.imshow("ImageBlur", imgBlur)
+    # cv2.imshow("ImageThreshold", imgThreshold)
+    # cv2.imshow("ImageThres", imgMedian)
+
+    if cv2.waitKey(60) & 0xFF == ord('q'):
+        break
+
+# Menutup koneksi port serial saat program berakhir
+ser.close()
