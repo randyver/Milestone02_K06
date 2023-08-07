d1b703744d544002ef5cd7a6373f6bbb0d7ff831
Thea <116005906+pandaandsushi@users.noreply.github.com>
2023-08-07 07:32:13 +0700

    color based reading loop

--- src/detection/main.py
+++ src/detection/main.py
@@ -44,7 +44,16 @@ while True:
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3,3), 1)
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
 
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+    kernel = np.ones((3, 3), np.int8)
+    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+
+    checkParkingSpace(imgDilate)
     cv2.imshow("Image", img)
     # cv2.imshow("ImageBlur", imgBlur)
     # cv2.imshow("ImageThreshold", imgThreshold)
